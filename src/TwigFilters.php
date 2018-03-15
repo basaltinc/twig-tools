5b603edbf01dd3bd6cadb8c1bcaf52ba79010ab8
Joe Karasek <joseph.karasek@gmail.com>
2018-03-14 20:37:31 -0700

    add comments

--- src/TwigFilters.php
+++ src/TwigFilters.php
@@ -6,6 +6,10 @@ use \Twig_SimpleFilter;
 
 class TwigFilters {
 
+  /**
+   * Creates a twig filter that removes `null` from an array.
+   * @return \Twig_SimpleFilter
+   */
   public static function remove_null($name = 'remove_null') {
     return new Twig_SimpleFilter($name, function ($array) {
       if (!is_array($array)) {
